d09af4d8e6762e5fbd90208013465433b1308184
Bijay Rauniyar <rauniyarbijay117@gmail.com>
2019-09-02 17:30:28 +0545

    react boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+
+import store from './store';
+import App from './components/App';
+import reset from './constants/css/reset';
+
+const GlobalStyle = createGlobalStyle`${reset}`;
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+    <BrowserRouter>
+        <Fragment>
+            <Provider store={store}>
+                <App />
+            </Provider>
+            <GlobalStyle />
+        </Fragment>
+    </BrowserRouter>,
+    document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,15 @@
-const path = require('path');
-//const HtmlWebPackPlugin = require("html-webpack-plugin");
+    
+require('@babel/register');
+const webpackMerge = require('webpack-merge');
 
-// const htmlPlugin = new HtmlWebPackPlugin({
-//     template: "./src/index.html",
-//     filename: "./index.html"
+const common = require('./config/webpack/webpack.common.babel');
 
-module.exports = {
-    entry:'./src/index.js',
-    output:{
-        path:path.join(__dirname,'public'),
-        filename:'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
-        }
-      ]
-    },
-    devServer:{
-      contentBase:path.join(__dirname,'public')
-    }
-    //mode:'development',
-    //plugins: [htmlPlugin]
+const envs = {
+    development: 'dev',
+    production: 'prod'
 };
+
+/* eslint-disable global-require,import/no-dynamic-require */
+const env = envs[process.env.NODE_ENV || 'development'];
+const envConfig = require(`./config/webpack/webpack.${env}.babel`);
+module.exports = webpackMerge(common, envConfig);
